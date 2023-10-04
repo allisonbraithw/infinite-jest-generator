45c9a4d2994a27eb03d8eaac462c155b11ec8a60
Allison Braithwaite <alliroseb@gmail.com>
2023-10-03 21:29:44 -0700

    start on dockerfiles

--- graphene-backend/app/robot/character_query.py
+++ graphene-backend/app/robot/character_query.py
@@ -15,11 +15,13 @@ from sentence_transformers import SentenceTransformer
 openai.organization = os.environ.get("OPENAI_ORG")
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
+
 def get_character_description_summary(docs: list, character: str = "Hal Incandenza"):
     # pass results to OpenAI & ask it to summarize
     system_prompt = f"You are a helpful AI assistant that takes chunks of text about a character \
         and produces a summary of that character's physical appearance. You should extract and \
-        summarize physical descriptors only, ignore non-physical details about the character. \
+        summarize physical descriptors only, ignore non-physical details about the character, and ignore \
+        descriptions of other characters not specified in the prompt.def\
         ```{docs}```"
     messages = [
         {"role": "system", "content": f"{system_prompt}"},
