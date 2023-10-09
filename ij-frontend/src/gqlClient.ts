f03529dbd0c8392999141d918181b0cdb26aedc4
Allison Braithwaite <alliroseb@gmail.com>
2023-10-08 19:47:04 -0700

    env var define url

--- ij-frontend/src/gqlClient.ts
+++ ij-frontend/src/gqlClient.ts
@@ -4,8 +4,9 @@ import { getMainDefinition } from "@apollo/client/utilities";
 import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
 import { createClient } from "graphql-ws";
 
-const BASE_GRAPHQL_URL = "http://localhost:8000/graphql";
-const BASE_GRAPHQL_SUBSCRIPTIONS_URL = "ws://localhost:8080/graphql";
+const BASE_GRAPHQL_URL = import.meta.env.VITE_BASE_GRAPHQL_URL;
+const BASE_GRAPHQL_SUBSCRIPTIONS_URL = import.meta.env
+  .VITE_BASE_GRAPHQL_SUBSCRIPTIONS_URL;
 
 const httpLink = new HttpLink({
   uri: BASE_GRAPHQL_URL,
