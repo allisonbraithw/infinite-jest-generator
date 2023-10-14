f39e382e86b7636abffb626ceada452c6e1db8f3
Allison Braithwaite <alliroseb@gmail.com>
2023-10-14 14:55:05 -0700

    more setup

--- graphene-backend/app/dependency_factory.py
+++ graphene-backend/app/dependency_factory.py
@@ -1,4 +1,5 @@
 import chromadb
+import weaviate
 import os
 
 
@@ -6,6 +7,7 @@ class DependencyFactory:
 
     # Service Clients
     _chroma_client = None
+    _weaviate_client = None
 
     @property
     def chroma_client(self):
@@ -13,6 +15,19 @@ class DependencyFactory:
             self._chroma_client = chromadb.HttpClient(host=os.environ.get(
                 "CHROMA_HOST"), port=os.environ.get("CHROMA_PORT"))
         return self._chroma_client
+
+    @property
+    def weaviate_client(self):
+        if self._weaviate_client is None:
+            auth_config = weaviate.AuthApiKey(
+                api_key=os.environ.get("WEAVIATE_API_KEY"))
+            self._weaviate_client = weaviate.Client(
+                url=os.environ.get("WEAVIATE_HOST"),
+                auth_client_secret=auth_config,
+                additional_headers={
+                    "X-OpenAI-Api-Key": os.environ.get("OPENAI_API_KEY")
+                })
+        return self._weaviate_client
 
 
 dependency_factory = DependencyFactory()
--- graphene-backend/app/main.py
+++ graphene-backend/app/main.py
@@ -11,13 +11,14 @@ import google.cloud.logging
 
 from schema.schema import schema
 from vector_management.text_processing import load_or_open_chunks_and_pages
+from vector_management.weaviate import initialize_schema, configure_batches, ChunkType
 from vector_management.chroma import initialize_collection
 from dependency_factory import dependency_factory as df
 
 load_dotenv()
 
 
-def initialize_vectordb():
+def initialize_chroma_vectordb():
     logging.info("Hello here i am")
     chunks, pages = load_or_open_chunks_and_pages(root_dir="./data/")
     logging.info(f"Got {len(chunks)} chunks")
@@ -26,6 +27,16 @@ def initialize_vectordb():
         initialize_collection(df.chroma_client, pages)
     except Exception:
         return "Error initializing collection", 500
+    return "OK", 200
+
+
+def initialize_wv_vectordb():
+    chunks, pages = load_or_open_chunks_and_pages(root_dir="./data/")
+    try:
+        configure_batches(chunks, type=ChunkType.CHUNK)
+        configure_batches(pages, type=ChunkType.PAGE)
+    except Exception as e:
+        return f"Error configuring batches: {e}", 500
     return "OK", 200
 
 
@@ -50,7 +61,15 @@ class App:
             logging_client.setup_logging()
 
         # Initialize collection
-        initialize_vectordb()
+        match os.environ.get("VECTORDB"):
+            case "chroma":
+                initialize_chroma_vectordb()
+            case "weaviate":
+                initialize_schema()
+                app.add_url_rule(
+                    '/reinitialize_vectordb',
+                    view_func=initialize_wv_vectordb
+                )
         self.app = app
 
     def run(self, port):
